689e6225dddec601c2acc72052f88d0ed93c4487
lostf1sh <slorixbaba@outlook.com>
2026-04-30 05:13:20 +0300

    Update Zed icon SVG in StatusSection for improved accuracy

--- src/components/StatusSection.vue
+++ src/components/StatusSection.vue
@@ -123,8 +123,8 @@ const statusDotClass = computed(() => {
                 "
                 class="flex items-center gap-3"
             >
-                <svg class="w-3.5 h-3.5 flex-shrink-0 text-catppuccin-blue" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                    <path d="M24 1.61v20.78L18.36 24 0 17.61l5.64 2.22L0 21.39l18.36 2.22L24 21.39V1.61L18.36 0 0 6.39l5.64-2.22L0 2.61 18.36.39 24 1.61zM9.13 13.99l1.95-1.95-4.58-4.58-1.95 1.95 4.58 4.58zm5.11-5.11l1.95-1.95-6.69-6.69-1.95 1.95 6.69 6.69zm-2.63 2.63l1.95-1.95-4.58-4.58-1.95 1.95 4.58 4.58zm5.11-5.11l1.95-1.95-6.69-6.69-1.95 1.95 6.69 6.69z"/>
+                <svg class="w-3.5 h-3.5 flex-shrink-0 text-catppuccin-blue" viewBox="0 0 96 96" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M9 6a3 3 0 0 0-3 3v66H0V9a9 9 0 0 1 9-9h80.379c4.009 0 6.016 4.847 3.182 7.682L43.055 57.187H57V51h6v7.688a4.5 4.5 0 0 1-4.5 4.5H37.055L26.743 73.5H73.5V36h6v37.5a6 6 0 0 1-6 6H20.743L10.243 90H87a3 3 0 0 0 3-3V21h6v66a9 9 0 0 1-9 9H6.621c-4.009 0-6.016-4.847-3.182-7.682L52.757 39H39v6h-6v-7.5a4.5 4.5 0 0 1 4.5-4.5h21.257l10.5-10.5H22.5V60h-6V22.5a6 6 0 0 1 6-6h52.757L85.757 6H9Z"/>
                 </svg>
                 <span class="text-catppuccin-subtle">{{
                     editorStatus.name === "Zed" ? "zed" : "code"
